fc8b986d19f399c5e4b1e2faefa99808718d2fef
BenjaminYde <benjamin.yde@gmail.com>
2022-01-23 17:26:08 +0100

    - added game builder script

---
+++ Assets/Scripts/GameBuilder.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace UnityTemplateProjects
+{
+    public class GameBuilder
+    {
+        public static void PerformBuildWindows()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+            buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+            buildPlayerOptions.locationPathName = "build/Windows";
+            buildPlayerOptions.options = BuildOptions.None;
+
+            BuildReport buildReport = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary buildSummary = buildReport.summary;
+
+            if (buildSummary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build success!");
+            }
+            else
+            {
+                Debug.Log("Build failed!");
+            }
+        }
+    }
+}
